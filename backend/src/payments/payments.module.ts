c5be605c2c0bc83a4c79e9121daef9ef3104ec1d
Sumeet Koyande <sumeet.k@octanom.com>
2025-12-14 22:53:26 +0530

    Update database name in .env.example and import SubscriptionsModule in PaymentsModule

--- backend/src/payments/payments.module.ts
+++ backend/src/payments/payments.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { SubscriptionsModule } from '../subscriptions/subscriptions.module';
 import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
 import { StripeWebhookController } from './stripe.webhook.controller';
@@ -6,5 +7,6 @@ import { StripeWebhookController } from './stripe.webhook.controller';
 @Module({
   providers: [PaymentsService],
   controllers: [PaymentsController, StripeWebhookController],
+  imports: [SubscriptionsModule],
 })
 export class PaymentsModule {}
